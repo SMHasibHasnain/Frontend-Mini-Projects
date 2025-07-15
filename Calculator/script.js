072883c715d2c8dc2a079548a62da5864540f743
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2025-07-15 20:39:23 +0600

    add calculation logic to the calculator.

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,16 +1,24 @@
 const buttons = document.querySelectorAll("button");
+const showCalc = document.querySelector(".calc");
+const showHistory = document.querySelector("p");
+const calcStack = [];
+let calcStr = "";
+let y;
+
 
 buttons.forEach( button => {
   const btnSound = new Audio("./audio/btn.mp3");
+  btnSound.volume = 1.0;
   button.addEventListener("mousedown", () => {
-    btnSound.play()
+    btnSound.play();
     button.classList.add("btn-clicked");
+    getInput(button.value);
   });  
   button.addEventListener("mouseup", () => {
     button.classList.remove("btn-clicked");
   });  
   button.addEventListener("touchstart", () => {
-    btnSound.play()
+    btnSound.play();
     button.classList.add("btn-clicked");
   });  
   button.addEventListener("touchend", () => {
@@ -18,7 +26,59 @@ buttons.forEach( button => {
   });  
 });
 
-// for(let i=0; i<buttons.length; i++) {
+
+
+function getInput(value) {
+  if(value == "=") {
+    calcStack.push(calcStr);
+    calcStr = "";
+    const x = showCalc.value;
+    showCalc.value = "";
+    doCalculate();
+    showHistory.innerText = x;
+  } else if(value == "*" || value == "+" || value == "-" || value =="/" || value=="%"){
+    calcStack.push(calcStr);
+    calcStr = "";
+    calcStack.push(value);
+    showCalc.value += value;
+  } else if(value == "C") {
+    calcStack.splice(0, calcStack.length);
+    calcStr = "";
+    showCalc.value = "";
+    showHistory.innerText = "";
+  } else {
+    showCalc.value += value;
+    calcStr += value;
+  }
+}
+
+function doCalculate() {
+
+  console.log(calcStack);
+
+  let cal = 0;
+  let calType = "";
   
-//   buttons[i].addEventListener("click", () => btnSound.play());
-// }
+  calcStack.forEach(element => {
+    if(element >= 0 || element <=9) {
+      if(calType == "+") {
+        cal += Number(element);
+      } else if(calType == "-") {
+        cal -= Number(element);
+      } else if(calType == "/") {
+        cal /= Number(element);
+      } else if(calType == "*") {
+        cal *= Number(element);
+      } else if(calType == "%") {
+        cal = Number(element) % cal;
+      } else {
+        cal = Number(element);
+      }
+    } else {
+      calType = element;
+    }
+  });
+
+  showCalc.value = cal;
+  console.log(showCalc.value);
+}
